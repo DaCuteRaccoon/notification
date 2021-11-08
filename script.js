635915016c22f698e555bcf0e808f4626542fd6e
DaCuteRaccoon <72467156+DaCuteRaccoon@users.noreply.github.com>
2021-11-08 12:47:41 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+// Notification.permission
 
+Notification.requestPermission().then(function (permission) {
+    console.log(permission);
+});
+
+
+var title = "JavaScript Jeep";
+icon = 'https://homepages.cae.wisc.edu/~ece533/images/airplane.png';
+var body = "It's Your boarding time";
+var notification = new Notification(title, { body, icon });
+
+var notification = new Notification('Travel');
+notification.close();
+
+
+// https://levelup.gitconnected.com/creating-browser-notification-in-javascript-79e91bfb76c8
